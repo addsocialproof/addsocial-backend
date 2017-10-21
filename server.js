8bb110239b2a7ec64368fa33a5081b593a158839
James Thibaudeau <jamesthibaudeau@gmail.com>
2017-10-21 19:19:39 +0000

    created project

---
+++ server.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.MLAB);
+
+
+var app = express();
+
+//body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//static files
+app.use('/static', express.static(path.join(__dirname, 'static')));
+
+//routes
+app.use('/api', require('./server/routes.js'));
+
+//route is * to handle browser refresh, when I find a better way this will change
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+//server start
+app.listen(process.env.PORT, process.env.IP, function(err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log('Listening at: ' + process.env.IP + ':' + process.env.PORT);
+});
